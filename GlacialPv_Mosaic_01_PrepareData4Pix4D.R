bdbf5e22d9fbc575b353365a72372c6f3c1e0968
Stacie Hardy <stacie.hardy@noaa.gov>
2021-12-22 09:57:17 -0800

    Update GlacialPv_Mosaic_01_PrepareData4Pix4D.R
    Update to reference new query name in DB

--- GlacialPv_Mosaic_01_PrepareData4Pix4D.R
+++ GlacialPv_Mosaic_01_PrepareData4Pix4D.R
@@ -41,7 +41,7 @@ con <- RPostgreSQL::dbConnect(PostgreSQL(),
                               user = Sys.getenv("pep_admin"), 
                               rstudioapi::askForPassword(paste("Enter your DB password for user account: ", Sys.getenv("pep_admin"), sep = "")))
 
-meta <- RPostgreSQL::dbGetQuery(con, paste("select * from surv_pv_gla.tbl_images_4processing where (survey_method_lku = \'M\' or survey_method_lku = \'T\') and survey_year::integer = ", survey_year,  sep = "")) %>%
+meta <- RPostgreSQL::dbGetQuery(con, paste("select * from surv_pv_gla.tbl_images_4processing_mosaic where (survey_method_lku = \'M\' or survey_method_lku = \'T\') and survey_year::integer = ", survey_year,  sep = "")) %>%
   mutate(ImageSurveyID = image_survey_id,
          ImagePath = ifelse(camera_view == 'C', paste(image_path, flight, "center_view", image_name, sep = "/"),
                              ifelse(camera_view == 'L', paste(image_path, flight, "left_view", image_name, sep = "/"), 
