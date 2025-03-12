fff661bb4107220000af53743a22d763293f767f
staciekoslovsky-noaa <stacie.koslovsky@noaa.gov>
2025-03-12 14:15:55 -0700

    Update GlacialPv_COCOA_05_QAQC_Duplicates.R
    Updated after review of QA/QC process

--- GlacialPv_COCOA_05_QAQC_Duplicates.R
+++ GlacialPv_COCOA_05_QAQC_Duplicates.R
@@ -17,7 +17,7 @@ install_pkg("tidyverse")
 
 # Run code -------------------------------------------------------
 
-# Extract data from DB ------------------------------------------------------------------
+# Extract data from DB 
 con <- RPostgreSQL::dbConnect(PostgreSQL(), 
                               dbname = Sys.getenv("pep_db"), 
                               host = Sys.getenv("pep_ip"), 
@@ -25,30 +25,30 @@ con <- RPostgreSQL::dbConnect(PostgreSQL(),
                               password = Sys.getenv("admin_pw"))
 
 # Create fields for tracking QA/QC, if it doesn't already exist
-RPostgreSQL::dbSendQuery(con, "ALTER TABLE surv_pv_gla.geo_detections 
-                         ADD COLUMN IF NOT EXISTS qaqc_duplicate CHARACTER VARYING(25)")
-RPostgreSQL::dbSendQuery(con, "UPDATE surv_pv_gla.geo_detections 
-                         SET qaqc_duplicate = \'to_evaluate\'")
+RPostgreSQL::dbSendQuery(con, "ALTER TABLE surv_pv_gla.geo_detections ADD COLUMN IF NOT EXISTS qaqc_duplicate CHARACTER VARYING(25)")
+RPostgreSQL::dbSendQuery(con, "UPDATE surv_pv_gla.geo_detections SET qaqc_duplicate = \'to_evaluate\'")
 
-RPostgreSQL::dbSendQuery(con, "ALTER TABLE surv_pv_gla.tbl_detections_processed_rgb 
-                         ADD COLUMN IF NOT EXISTS qaqc_duplicate CHARACTER VARYING(25)")
-RPostgreSQL::dbSendQuery(con, "UPDATE surv_pv_gla.tbl_detections_processed_rgb 
-                         SET qaqc_duplicate = \'to_evaluate\'")
+RPostgreSQL::dbSendQuery(con, "ALTER TABLE surv_pv_gla.tbl_detections_processed_rgb ADD COLUMN IF NOT EXISTS qaqc_duplicate CHARACTER VARYING(25)")
+RPostgreSQL::dbSendQuery(con, "UPDATE surv_pv_gla.tbl_detections_processed_rgb SET qaqc_duplicate = \'to_evaluate\'")
 
 # Mark "suppressed" detections as such in qaqc_duplicate field in tbl_detections_processed_rgb and geo_detections
-RPostgreSQL::dbSendQuery(con, "UPDATE surv_pv_gla.geo_detections
-                         SET qaqc_duplicate = \'suppressed\' 
-                         WHERE suppressed = \'true\'")
+RPostgreSQL::dbSendQuery(con, "UPDATE surv_pv_gla.geo_detections SET qaqc_duplicate = \'suppressed\' WHERE suppressed = \'true\'")
 
-RPostgreSQL::dbSendQuery(con, "UPDATE surv_pv_gla.tbl_detections_processed_rgb
-                         SET qaqc_duplicate = \'suppressed\' 
+RPostgreSQL::dbSendQuery(con, "UPDATE surv_pv_gla.tbl_detections_processed_rgb SET qaqc_duplicate = \'suppressed\' 
                          WHERE detection_id IN (SELECT detection_id FROM surv_pv_gla.geo_detections WHERE suppressed = \'true\')")
 
+# Create inxed on detection_id field to streamline processing
+RPostgreSQL::dbSendQuery(con, "CREATE INDEX IF NOT EXISTS index_detection_id ON surv_pv_gla.geo_detections(detection_id)")
+RPostgreSQL::dbSendQuery(con, "CREATE INDEX IF NOT EXISTS index_detection_id ON surv_pv_gla.tbl_detections_processed_rgb(detection_id)")
+
 # Identify cross-camera view duplicates and mark as such in tbl_detections_processed_rgb and geo_detections
-#### Ideally from qaqc_cocoa_xcamera_duplicates query
+  ## These queries take a while to run...
+RPostgreSQL::dbSendQuery(con, "UPDATE surv_pv_gla.geo_detections SET qaqc_duplicate = \'xcamera_dupe\' 
+                         WHERE detection_id IN (SELECT detection_id FROM surv_pv_gla.qaqc_cocoa_xcamera_duplicates)")
 
+RPostgreSQL::dbSendQuery(con, "UPDATE surv_pv_gla.tbl_detections_processed_rgb SET qaqc_duplicate = \'xcamera_dupe\' 
+                         WHERE detection_id IN (SELECT detection_id FROM surv_pv_gla.qaqc_cocoa_xcamera_duplicates)")
 
-
-# Disconnect for database and delete unnecessary variables ------------------------------
+# Disconnect for database and delete unnecessary variables 
 dbDisconnect(con)
 rm(con)
