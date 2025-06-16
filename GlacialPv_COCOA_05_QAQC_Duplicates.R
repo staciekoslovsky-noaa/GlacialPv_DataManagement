4f1ca0f03a80a2d0b490f423606ffc096f951820
staciekoslovsky-noaa <stacie.koslovsky@noaa.gov>
2025-06-16 16:29:38 -0700

    Update GlacialPv_COCOA_05_QAQC_Duplicates.R

--- GlacialPv_COCOA_05_QAQC_Duplicates.R
+++ GlacialPv_COCOA_05_QAQC_Duplicates.R
@@ -26,16 +26,24 @@ con <- RPostgreSQL::dbConnect(PostgreSQL(),
 
 # Create fields for tracking QA/QC, if it doesn't already exist
 RPostgreSQL::dbSendQuery(con, "ALTER TABLE surv_pv_gla.geo_detections ADD COLUMN IF NOT EXISTS qaqc_duplicate CHARACTER VARYING(25)")
-RPostgreSQL::dbSendQuery(con, "UPDATE surv_pv_gla.geo_detections SET qaqc_duplicate = \'to_evaluate\'")
+RPostgreSQL::dbSendQuery(con, "UPDATE surv_pv_gla.geo_detections SET qaqc_duplicate = NULL")
+RPostgreSQL::dbSendQuery(con, "UPDATE surv_pv_gla.geo_detections SET qaqc_duplicate = \'na_latte\' WHERE detection_id LIKE \'%sample%\'")
+RPostgreSQL::dbSendQuery(con, "UPDATE surv_pv_gla.geo_detections SET qaqc_duplicate = \'to_evaluate\' WHERE qaqc_duplicate IS NULL")
 
 RPostgreSQL::dbSendQuery(con, "ALTER TABLE surv_pv_gla.tbl_detections_processed_rgb ADD COLUMN IF NOT EXISTS qaqc_duplicate CHARACTER VARYING(25)")
-RPostgreSQL::dbSendQuery(con, "UPDATE surv_pv_gla.tbl_detections_processed_rgb SET qaqc_duplicate = \'to_evaluate\'")
+RPostgreSQL::dbSendQuery(con, "UPDATE surv_pv_gla.tbl_detections_processed_rgb SET qaqc_duplicate = NULL")
+RPostgreSQL::dbSendQuery(con, "UPDATE surv_pv_gla.tbl_detections_processed_rgb SET qaqc_duplicate = \'na_latte\' WHERE detection_id LIKE \'%sample%\'")
+RPostgreSQL::dbSendQuery(con, "UPDATE surv_pv_gla.tbl_detections_processed_rgb SET qaqc_duplicate = \'to_evaluate\' WHERE qaqc_duplicate IS NULL")
+
+# Mark non-harbor seal detections as "not_applicable" in qaqc_duplicate field in tbl_detections_processed_rgb
+RPostgreSQL::dbSendQuery(con, "UPDATE surv_pv_gla.tbl_detections_processed_rgb SET qaqc_duplicate = \'na_not_seal\' WHERE detection_type NOT LIKE \'%harbor%\' AND detection_type NOT LIKE \'%seal%\'")
 
 # Mark "suppressed" detections as such in qaqc_duplicate field in tbl_detections_processed_rgb and geo_detections
-RPostgreSQL::dbSendQuery(con, "UPDATE surv_pv_gla.geo_detections SET qaqc_duplicate = \'suppressed\' WHERE suppressed = \'true\'")
+RPostgreSQL::dbSendQuery(con, "UPDATE surv_pv_gla.geo_detections SET qaqc_duplicate = \'dupe_suppressed\' WHERE suppressed = \'true\' AND qaqc_duplicate NOT LIKE \'na_%\'")
 
-RPostgreSQL::dbSendQuery(con, "UPDATE surv_pv_gla.tbl_detections_processed_rgb SET qaqc_duplicate = \'suppressed\' 
-                         WHERE detection_id IN (SELECT detection_id FROM surv_pv_gla.geo_detections WHERE suppressed = \'true\')")
+RPostgreSQL::dbSendQuery(con, "UPDATE surv_pv_gla.tbl_detections_processed_rgb SET qaqc_duplicate = \'dupe_suppressed\' 
+                         WHERE detection_id IN (SELECT detection_id FROM surv_pv_gla.geo_detections WHERE suppressed = \'true\')
+                         AND qaqc_duplicate NOT LIKE \'na_%\'")
 
 # Create index on detection_id field to streamline processing
 RPostgreSQL::dbSendQuery(con, "CREATE INDEX IF NOT EXISTS index_detection_id ON surv_pv_gla.geo_detections(detection_id)")
@@ -45,10 +53,10 @@ RPostgreSQL::dbSendQuery(con, "CREATE INDEX IF NOT EXISTS index_detection_id ON 
 dupes <- RPostgreSQL::dbGetQuery(con, "SELECT * FROM surv_pv_gla.qaqc_cocoa_xcamera_duplicates")
 
 for (i in 1:nrow(dupes)){
-  RPostgreSQL::dbSendQuery(con, paste0("UPDATE surv_pv_gla.geo_detections SET qaqc_duplicate = \'xcamera\' 
+  RPostgreSQL::dbSendQuery(con, paste0("UPDATE surv_pv_gla.geo_detections SET qaqc_duplicate = \'dupe_xcamera\' 
                          WHERE detection_id = \'", dupes$duplicate_detection[i], "\'"))
   
-  RPostgreSQL::dbSendQuery(con, paste0("UPDATE surv_pv_gla.tbl_detections_processed_rgb SET qaqc_duplicate = \'xcamera\' 
+  RPostgreSQL::dbSendQuery(con, paste0("UPDATE surv_pv_gla.tbl_detections_processed_rgb SET qaqc_duplicate = \'dupe_xcamera\' 
                          WHERE detection_id = \'", dupes$duplicate_detection[i], "\'"))
 }
 
